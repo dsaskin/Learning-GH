055f25decd4f5d6e62f1f8908fa2349da1ee2dbe
DAskin <debbie.askin@askinsupport.com>
2023-05-03 11:06:08 -0400

    Initial Commit

---
+++ LearnGithub/LearnGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnGithubApp.swift
+//  LearnGithub
+//
+//  Created by DEBBIE ASKIN on 5/3/23.
+//
+
+import SwiftUI
+
+@main
+struct LearnGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
